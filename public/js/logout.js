9800a1b36cf8532f07f0e1d9f5bbe151029a55c6
Jake Sadowski <110630130+jsadowski7@users.noreply.github.com>
2022-12-20 20:38:18 -0600

    Added login.js and logout.js content, entered user_id into key for model index.js

---
+++ public/js/logout.js
@@ -0,0 +1,16 @@
+// Send POST request
+const logout = async () => {
+    const response = await fetch('/api/users/logout', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+    });
+    // If ok redirect to homepage
+    if (response.ok) {
+      document.location.replace('/');
+    } else {
+      alert(response.statusText);
+    }
+  };
+  
+  document.querySelector('#logout').addEventListener('click', logout);
+  
