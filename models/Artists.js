a8876dac49e6c0372e8d764e2d28fe613f879410
Marcie Lucke <marcielucke@Marcies-MacBook-Pro.local>
2022-12-22 18:56:09 -0600

    fix artist model

--- models/Artists.js
+++ models/Artists.js
@@ -1,7 +1,7 @@
 const { Model, DataTypes } = require('sequelize');
 const bcrypt = require('bcrypt');
 const sequelize = require('../config/connection');
-const { Artists } = require('.');
+
 
 
 class Artists extends Model {}
@@ -52,9 +52,9 @@ Artists.init(
         timestamps: false,
         freezeTableName: true,
         underscored: true,
-        modelName: 'project',
+        modelName: 'artists',
       }
     );
-user_id
+
 
 module.exports = Artists;
