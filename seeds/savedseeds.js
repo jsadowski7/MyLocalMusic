e414f2dfb77d9d3781bf0d6bd6e3f9c05175e1fb
Marcie Lucke <marcielucke@Marcies-MacBook-Pro.local>
2022-12-22 19:46:00 -0600

    api/seeds/util

--- seeds/savedseeds.js
+++ seeds/savedseeds.js
@@ -1,20 +1,20 @@
-const { Saved } = require('../models');
+const { Saved, User } = require('../models');
 
 const savedData = [
   {
     artist: '',
     genre: 14.99,
-    user_id: 1
+  
   },
   {
     artist: 'Plain T-Shirt',
     genre: 14.99,
-    user_id: 2
+   
   },
   {
     artist: 'Plain T-Shirt',
     genre: 14.99,
-    user_id: 3
+   
   },
   
  
