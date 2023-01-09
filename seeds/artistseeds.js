caf7a65df74209afc832216e15d03fad3f1355b5
Marcie Lucke <marcielucke@Marcies-MacBook-Pro.local>
2023-01-09 12:11:49 -0600

    seeds file artists

--- seeds/artistseeds.js
+++ seeds/artistseeds.js
@@ -9,7 +9,7 @@ const artist_seeds = [
         artist_site: 'https://www.sweetspiritatx.com/',
         spotify:
             'https://open.spotify.com/artist/32L6HnQe9jCXLmjpnS2XSQ',
-        genre_id: 1,
+       
       
     },
 
@@ -21,7 +21,7 @@ const artist_seeds = [
         artist_site: 'https://www.nightcapatx.com/',
         spotify:
             'https://open.spotify.com/artist/0d4OObbLXdSSO2suAeC4jk',
-        genre_id: 1,
+      
     },
     {
         band_name: 'The Peterson Brothers',
@@ -29,7 +29,7 @@ const artist_seeds = [
         description: 'The Peterson Brothers combine infectious energy and modern grooves with jazz, funk, blues & soul to a create their own unique sound.',
         artist_site: 'https://www.petersonbrothersband.com/',
         spotify: 'https://open.spotify.com/artist/6op6sWjJ0fTmCg4bPyDkJs',
-        genre_id:3,
+       
     },
     {
         band_name: 'Explosions in the Sky',
@@ -37,7 +37,7 @@ const artist_seeds = [
         description: 'Instrumental rock band formed in 1999. The band has garnered popularity for their elaborately developed guitar work, narratively styled instrumentals, and enthusiastic live shows',
         artist_site: 'http://www.explosionsinthesky.com/',
         spotify: 'https://open.spotify.com/artist/1uQWmt1OhuHGRKmZ2ZcL6p',
-        genre_id: 3,
+       
     },
     {
         band_name: 'The Sword',
@@ -45,7 +45,7 @@ const artist_seeds = [
         description: 'A doom rock/metal band formed in 2003 in ATX had a song featured on video game Guitar Hero II',
         artist_site: 'https://theswordofficial.com/',
         spotify: 'https://open.spotify.com/artist/0q32a3GRCjDxS4EIrC7YVY',
-        genre_id: 3,
+       
     },
     {
         band_name: 'Gary Clark Jr.',
@@ -53,7 +53,7 @@ const artist_seeds = [
         description: 'Rock/Blues artist born in Austin. He is known for his fusion of blues, rock and soul music with elements of hip hop.',
         artist_site: 'https://www.garyclarkjr.com/?frontpage=true',
         spotify: 'https://open.spotify.com/artist/01aC2ikO4Xgb2LUpf9JfKp',
-        genre_id: 2,
+      
     },
 ];
 
